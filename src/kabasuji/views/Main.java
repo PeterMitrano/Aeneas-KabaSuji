ebfafca7d991dd3b90862ba8a0ffd0a26c13ca5d
Peter_Mitrano <mitranopeter@gmail.com>
2016-04-01 00:17:21 -0400

    Menu is causing problems, but everything runs
    need to add a proper controller to the toolbar

--- src/kabasuji/views/Main.java
+++ src/kabasuji/views/Main.java
@@ -3,11 +3,16 @@ package kabasuji.views;
 import java.io.IOException;
 
 import com.jfoenix.controls.JFXDecorator;
+import com.jfoenix.controls.JFXPopup;
+import com.jfoenix.controls.JFXPopup.PopupHPosition;
+import com.jfoenix.controls.JFXPopup.PopupVPosition;
+import com.jfoenix.controls.JFXRippler;
 
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -16,10 +21,35 @@ public class Main extends Application {
 	@FXML
 	private StackPane root;
 
+	@FXML
+	private StackPane optionsBurger;
+	@FXML
+	private JFXRippler optionsRippler;
+	
+	@FXML
+	private Label about;
+
+	@FXML
+	private Label help;
+
+	@FXML
+	private JFXPopup toolbarPopup;
+
 	public void start(Stage stage) {
 		try {
 			root = FXMLLoader.load(getClass().getResource("/resources/fxml/Main.fxml"));
-
+			
+			// init Popup
+			toolbarPopup = (JFXPopup) root.lookup("#toolbarPopup");
+			optionsRippler = (JFXRippler) root.lookup("#optionsRippler");
+			optionsBurger = (StackPane) root.lookup("#optionsBurger");
+			
+			toolbarPopup.setPopupContainer(root);
+			toolbarPopup.setSource(optionsRippler);			
+			optionsBurger.setOnMouseClicked((e) -> {
+				toolbarPopup.show(PopupVPosition.TOP, PopupHPosition.RIGHT, -12, 15);
+			});
+			
 			Scene scene = new Scene(new JFXDecorator(stage, root), 800, 800);
 
 			scene.getStylesheets().add(getClass().getResource("/resources/css/jfoenix-fonts.css").toExternalForm());
--- src/kabasuji/views/MainView.java
+++ src/kabasuji/views/MainView.java
@@ -3,30 +3,43 @@ package kabasuji.views;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import com.jfoenix.controls.JFXPopup;
+import com.jfoenix.controls.JFXPopup.PopupHPosition;
+import com.jfoenix.controls.JFXPopup.PopupVPosition;
+import com.jfoenix.controls.JFXRippler;
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.StackPane;
 
 public class MainView implements Initializable {
 
-	@FXML
-	private StackPane content;
+  @FXML
+  private StackPane content;
 
-	@FXML
-	private WelcomeView welcomeView;
+  @FXML
+  private StackPane optionsBurger;
+  @FXML
+  private JFXRippler optionsRippler;
 
-	@FXML
-	private ViewAchievementsView viewAchievementsView;
+  @FXML
+  private JFXPopup toolbarPopup;
 
-	private void switchToWelcomeView() {
-		content.getChildren().clear();
-		content.getChildren().add(welcomeView);
-	}
+  @FXML
+  private WelcomeView welcomeView;
 
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
+  @FXML
+  private ViewAchievementsView viewAchievementsView;
+
+  private void switchToWelcomeView() {
+    content.getChildren().clear();
+    content.getChildren().add(welcomeView);
+  }
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
     welcomeView = new WelcomeView(this);
 
-		switchToWelcomeView();
-	}
+    switchToWelcomeView();
+  }
 }
