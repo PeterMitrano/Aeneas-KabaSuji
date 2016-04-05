f145ee1f4a815a29da699721694027c8e71a5bf3
Peter_Mitrano <mitranopeter@gmail.com>
2016-04-05 04:44:15 -0400

    Started first attempt at an actual piece
    Using a AnchorPane, and Rectangle nodes
    Not sure if this will drag & drop nicely
    however, it has the advnatge that it can be build easily from a
    PieceModel

--- src/kabasuji/views/BuildLevelView.java
+++ src/kabasuji/views/BuildLevelView.java
@@ -2,20 +2,26 @@ package kabasuji.views;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import com.jfoenix.controls.JFXListView;
-import com.jfoenix.controls.JFXRippler;
 import com.jfoenix.effects.JFXDepthManager;
 
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.RectangleBuilder;
 
 import kabasuji.models.Model;
 
@@ -36,8 +42,7 @@ public class BuildLevelView extends BorderPane implements Initializable {
       loader.setRoot(this);
       loader.setController(this);
       loader.load();
-    }
-    catch (IOException e){
+    } catch (IOException e) {
       e.printStackTrace();
     }
   }
@@ -45,6 +50,27 @@ public class BuildLevelView extends BorderPane implements Initializable {
   @Override
   public void initialize(URL location, ResourceBundle resources) {
     JFXDepthManager.setDepth(bullpenListView, 1);
+
+    // add a piece to the bullpen as an example
+    AnchorPane piecePane = new AnchorPane();
+    piecePane.setPrefSize(96,96);
+
+    Rectangle square1 = RectangleBuilder.create().width(16).height(16).x(0).y(0).fill(Color.BLUE).build();
+    Rectangle square2 = RectangleBuilder.create().width(16).height(16).x(16).y(0).fill(Color.BLUE).build();
+    Rectangle square3 = RectangleBuilder.create().width(16).height(16).x(16).y(16).fill(Color.BLUE).build();
+    Rectangle square4 = RectangleBuilder.create().width(16).height(16).x(0).y(16).fill(Color.BLUE).build();
+    Rectangle square5 = RectangleBuilder.create().width(16).height(16).x(0).y(32).fill(Color.BLUE).build();
+    Rectangle square6 = RectangleBuilder.create().width(16).height(16).x(32).y(16).fill(Color.BLUE).build();
+
+    piecePane.getChildren().add(square1);
+    piecePane.getChildren().add(square2);
+    piecePane.getChildren().add(square3);
+    piecePane.getChildren().add(square4);
+    piecePane.getChildren().add(square5);
+    piecePane.getChildren().add(square6);
+
+    List<AnchorPane> values = Arrays.asList(piecePane);
+    bullpenListView.setItems(FXCollections.observableList(values));
   }
 
 }
