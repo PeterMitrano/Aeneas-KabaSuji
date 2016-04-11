196558c86e6f8a1030893b596002b30747e4ae9c
Joseph Martin <josephm@maine.rr.com>
2016-04-11 16:27:56 -0400

    Add ReleaseNumber; add some docs

--- src/kabasuji/models/Level.java
+++ src/kabasuji/models/Level.java
@@ -10,6 +10,15 @@ public abstract class Level {
     this.bullpen = bullpen;
   }
   
+  /**
+   * Check if the level is done
+   * @return true if the level is complete, false otherwise.
+   */
   public abstract boolean isComplete();
+  
+  /**
+   * Get the board for this level
+   * @return The board used by this level.
+   */
   public abstract Board getBoard();
 }
--- src/kabasuji/models/ReleaseLevel.java
+++ src/kabasuji/models/ReleaseLevel.java
@@ -1,12 +1,16 @@
 package kabasuji.models;
 
+import java.util.ArrayList;
+
 public class ReleaseLevel extends Level {
   public static final String helpText = "";
   
   ReleaseBoard board;
+  ArrayList<ReleaseNumber> numbers;
   
-  public ReleaseLevel(Bullpen bullpen) {
+  public ReleaseLevel(Bullpen bullpen, ArrayList<ReleaseNumber> numbers) {
     super(bullpen);
+    this.numbers = numbers;
   }
   
   @Override
