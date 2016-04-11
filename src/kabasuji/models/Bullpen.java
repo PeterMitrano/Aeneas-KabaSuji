c02bd72837764836a7d3c50fbbbad595bb7cfbb8
Joseph Martin <josephm@maine.rr.com>
2016-04-11 15:35:47 -0400

    add noarg constructor to Bullpen

--- src/kabasuji/models/Bullpen.java
+++ src/kabasuji/models/Bullpen.java
@@ -5,6 +5,10 @@ import java.util.ArrayList;
 public class Bullpen {
   ArrayList<Piece> pieces;
   
+  public Bullpen() {
+    this.pieces = new ArrayList<Piece>();
+  }
+  
   public Bullpen(ArrayList<Piece> pieces) {
     this.pieces = pieces;
   }
