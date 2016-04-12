d46cedd1417f3fac6630c3612755e42fe3b223a1
Peter_Mitrano <mitranopeter@gmail.com>
2016-04-11 22:06:23 -0400

    added first test case

--- src/test/java/aeneas/models/ModelTest.java
+++ src/test/java/aeneas/models/ModelTest.java
@@ -1,5 +1,7 @@
 package aeneas.models;
 
+import static org.junit.Assert.*;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,6 +19,7 @@ public class ModelTest {
   @Test
   public void test() {
     Model m = new Model();
+    assertEquals("level count", m.levels.size(), 15);
   }
 
 }
