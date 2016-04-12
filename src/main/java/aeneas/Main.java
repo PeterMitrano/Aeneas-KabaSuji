1c9a79985062b1768947ed13d3a15d871c1868cc
Peter_Mitrano <mitranopeter@gmail.com>
2016-04-11 21:56:54 -0400

    moved resources again to match convention
    this is nice because it means we can use realtive paths for loading fxml

--- src/main/java/aeneas/Main.java
+++ src/main/java/aeneas/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
   public void start(Stage stage) {
     MainView mainView = new MainView(stage);
     Scene scene = new Scene(new JFXDecorator(stage, mainView.root), 800, 800);
-    scene.getStylesheets().add(getClass().getResource("/resources/css/kabasuji.css").toExternalForm());
+    scene.getStylesheets().add(getClass().getResource("kabasuji.css").toExternalForm());
     stage.setMinWidth(700);
     stage.setMinHeight(700);
     stage.setScene(scene);
