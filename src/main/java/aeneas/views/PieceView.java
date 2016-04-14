69b5a890a43024fd4fe1899836b434fea21b1dc3
Peter_Mitrano <mitranopeter@gmail.com>
2016-04-14 16:59:41 -0400

    reorganized by adding bullpen
    reduced code duplication between build and play level
    tests to come in next commit

--- src/main/java/aeneas/views/PieceView.java
+++ src/main/java/aeneas/views/PieceView.java
@@ -3,7 +3,14 @@ package aeneas.views;
 import aeneas.models.Piece;
 import aeneas.models.Square;
 
+import javafx.scene.SnapshotParameters;
+import javafx.scene.image.Image;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 
 public class PieceView extends Pane {
 
@@ -17,5 +24,25 @@ public class PieceView extends Pane {
       view.setY(s.getCol() * squareSize);
       getChildren().add(view);
     }
+
+    this.setOnDragDetected((MouseEvent event) -> {
+      Dragboard db = this.startDragAndDrop(TransferMode.MOVE);
+      ClipboardContent content = new ClipboardContent();
+
+      //this is what we use to get the object on the other end
+      content.putString(this.toString());
+
+      db.setContent(content);
+
+      SnapshotParameters snapshotParameters = new SnapshotParameters();
+      snapshotParameters.setFill(Color.TRANSPARENT);
+
+      PieceView fullSizedPieceView = new PieceView(pieceModel, BoardView.SQUARE_SIZE);
+
+      Image snapshotImage = fullSizedPieceView.snapshot(snapshotParameters, null);
+      db.setDragView(snapshotImage);
+
+      event.consume();
+    });
   }
 }
