f86b989641cc27cb23f052b3e5516440c8dc74fa
Peter_Mitrano <mitranopeter@gmail.com>
2016-04-16 16:31:46 -0400

    Addeding javadoc publishing to gh-pages via travis
    the key in the travis file is an encripyed key I generated

---
+++ .travis-scripts/push-javadocs-to-gh-pages.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ "$TRAVIS_REPO_SLUG" == "CheddarPuffs/Aeneas-Kabasuji" ] && [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+
+  # Get to the Travis build directory, configure git and clone the repo
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
+  git clone --branch=gh-pages https://${GH_TOKEN}@github.com/CheddarPuffs/AeneasKabasuji gh-pages
+
+  # Commit and Push the Changes
+  cd gh-pages
+  ./gradlew javadocs
+  git add --all .
+  git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+  git push -f origin gh-pages
+
+  echo -e "Published Javadoc to gh-pages.\n"
+
+fi
