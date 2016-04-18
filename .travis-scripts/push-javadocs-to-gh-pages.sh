d51001d7f41fd5e9fbb4a1fbfa8c6f81d17a703f
Peter_Mitrano <mitranopeter@gmail.com>
2016-04-18 17:59:21 -0400

    change url for testing

--- .travis-scripts/push-javadocs-to-gh-pages.sh
+++ .travis-scripts/push-javadocs-to-gh-pages.sh
@@ -8,7 +8,7 @@ if [ "$TRAVIS_REPO_SLUG" == "Cheddarpuffs/Aeneas-KabaSuji" ] && [ "$TRAVIS_JDK_V
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
-  git clone --branch=gh-pages https://${GH_TOKEN}@github.com/CheddarPuffs/Aeneas-Kabasuji gh-pages
+  git clone --branch=gh-pages https://${GH_TOKEN}@github.com/PeterMitrano/Aeneas-Kabasuji gh-pages
 
   # Commit and Push the Changes
   cd gh-pages
