25bc2fb827a50366a2a5a02265dd2bacf8f9d130
Peter_Mitrano <mitranopeter@gmail.com>
2016-04-24 05:26:09 -0400

    using static libs instead of gradle dependencies
    this allows eclipse to run things too

--- src/test/java/aeneas/views/NavigationTest.java
+++ src/test/java/aeneas/views/NavigationTest.java
@@ -1,18 +1,11 @@
 package aeneas.views;
 
-import static org.junit.Assert.*;
-
 import java.util.concurrent.TimeUnit;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-
 import org.testfx.framework.junit.ApplicationTest;
 
-import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
 import javafx.stage.Stage;
 
@@ -41,26 +34,5 @@ public class NavigationTest extends ApplicationTest {
 
     //hit back again to see if it breaks something
     clickOn("#back");
-
-    //test help and about
-    clickOn("#optionsBurger");
-
-    //move off the stupid scrollbar...
-    moveBy(-20,-10);
-    clickOn(MouseButton.PRIMARY);
-
-    //wait so humans can see stuff
-    sleep(1, TimeUnit.SECONDS);
-    clickOn("#accept");
-
-    clickOn("#optionsBurger");
-    moveBy(-20,25);
-
-    //move off the stupid scrollbar...
-    clickOn(MouseButton.PRIMARY);
-
-    //wait so humans can see stuff
-    sleep(1, TimeUnit.SECONDS);
-    clickOn("#accept");
   }
 }
