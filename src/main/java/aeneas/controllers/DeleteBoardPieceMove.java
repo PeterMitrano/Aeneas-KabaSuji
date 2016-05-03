c4476d4824fc1601a5c2eec23f3954232a43d1d5
garrisonhefter <gjhefter@gmail.com>
2016-05-02 21:30:15 -0400

    Trying some stuff out

---
+++ src/main/java/aeneas/controllers/DeleteBoardPieceMove.java
@@ -0,0 +1,56 @@
+package aeneas.controllers;
+import aeneas.models.Level;
+import aeneas.models.Level.LevelWithMoves;
+import aeneas.models.Piece;
+import aeneas.models.PlacedPiece;
+
+/**
+ * Move action to move a piece from the bullpen to the board
+ * @author Logan
+ * @author jbkuszmaul
+ */
+public class DeleteBoardPieceMove implements IMove {
+  Level level;
+
+  PlacedPiece placedPiece;
+
+  int row;
+  int col;
+
+  /**
+   * Constructor
+   *
+   * @param level The level that is currently being played
+   * @param piece The piece that is being moved
+   * @param row The row to place the piece in
+   * @param col The column to place the piece in
+   */
+  public DeleteBoardPieceMove(Level level, Piece piece, int row, int col) {
+    this.level = level;
+    this.placedPiece = new PlacedPiece(piece, row, col);
+    this.row = row;
+    this.col = col;
+  }
+
+  @Override
+  public boolean execute() {
+    if (!isValid()) return false;
+    if (level instanceof LevelWithMoves && level.isActive()) {
+      ((LevelWithMoves)level).decMoves();
+    }
+    return level.getBoard().addPiece(placedPiece);
+  }
+
+  @Override
+  public boolean undo() {
+    level.getBoard().removePiece(placedPiece);
+    level.getBullpen().addPiece(placedPiece.getPiece());
+    return true;
+  }
+
+  @Override
+  public boolean isValid() {
+    return level.getBoard().canAddPiece(placedPiece);
+  }
+
+}
--- src/main/java/aeneas/views/BuildLevelView.java
+++ src/main/java/aeneas/views/BuildLevelView.java
@@ -13,8 +13,10 @@ import com.jfoenix.controls.JFXListView;
 
 import aeneas.controllers.AddPieceMove;
 import aeneas.controllers.BoardSizeController;
+import aeneas.controllers.BullpenToBoardMove;
 import aeneas.controllers.ChangeLevelTypeMove;
 import aeneas.controllers.ChildDraggedListener;
+import aeneas.controllers.DeleteBoardPieceMove;
 import aeneas.controllers.IMove;
 import aeneas.controllers.UndoRedoController;
 import aeneas.controllers.ToggleTileMove;
@@ -38,6 +40,9 @@ import javafx.scene.control.RadioButton;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
@@ -232,7 +237,40 @@ public class BuildLevelView extends StackPane implements Initializable, RefreshL
       undoController.redoMove();
     });
 
+    this.setOnDragDropped((DragEvent event) -> {
+      Dragboard db = event.getDragboard();
+
+      // use this to draw the piece on the board
+      Piece piece = (Piece) db.getContent(Piece.dataFormat);
+      
+      DragSource source = model.getLatestDragSource();
+
+      if (source instanceof BoardView) {
+        IMove move = new DeleteBoardPieceMove(model, piece);
+      }
+
+      if (!move.execute()){
+        model.getLatestDragSource().returnPiece();
+      } else {
+        model.getLatestDragSource().dragSuccess();
+      }
+
+      refresh();
+
+      // this might change we we actually implement it,
+      // such as if they drop it on a square that doesn't exist
+      event.setDropCompleted(true);
+      event.consume();
+
+    });
+
+    // this is absolutely nessecary
+    this.setOnDragOver((DragEvent event) -> {
+      event.acceptTransferModes(TransferMode.MOVE);
+      event.consume();
+    });
   }
+  
 
   public void refresh() {
     isRefreshing = true;
